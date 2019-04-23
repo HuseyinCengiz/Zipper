3aec6cfc9cbd7c266093548cc8eb6fe90f21db9f
huseyin <hseyin.cengiz@hotmail.com>
2019-04-23 14:53:34 +0300

    checking if the file is binary or ascii was added.

--- src/main.c
+++ src/main.c
@@ -9,12 +9,10 @@
 
 #define HEADER_LENGTH 10000
 #define BODY_LINE_LENGTH 10000
-
 typedef enum boolean{
     false = 0,
     true = 1
 } bool;
-
 static char my_header[HEADER_LENGTH];
 
 int calculateSizeofFiles(char *fileNames[],int fileNumber) {
@@ -28,7 +26,6 @@ int calculateSizeofFiles(char *fileNames[],int fileNumber) {
     }
     return sum;
 }
-
 char ** getFileNames(IS is,int numberFiles){
     char ** files = (char **) malloc(sizeof(char *) * numberFiles);
     for(int i=0;i<numberFiles;i++){
@@ -79,6 +76,18 @@ char * readPermission(char * fileName){
     strcat(permission,"\0");
     return permission;
 }
+bool isBinaryFile(char * fileName){
+    struct stat fileStat;
+    if(stat(fileName,&fileStat) < 0){
+        printf("Dosya okumada hata!");
+        exit(EXIT_FAILURE);
+    }
+    if(fileStat.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)){
+        return true;
+    }else {
+        return false;
+    }
+}
 int readSize(char * fileName){
     struct stat fileStat;
     if(stat(fileName,&fileStat) < 0){
@@ -110,11 +119,21 @@ void writeToFile(char * fileName,char **fileNames,int numberFiles){
     }
     fclose(fp);
 }
+void checkIsBinary(char **fileNames,int numberFiles){
+    for(int i=0;i<numberFiles;i++){
+        if(isBinaryFile(fileNames[i]))
+        {
+            printf("%s giriş dosyası biçimi uyumsuz!",fileNames[i]);
+            exit(1);
+        }
+    }
+}
 bool zipControl(char **fileNames,int numberFiles){
     if(numberFiles>32){
         printf("Giriş dosyalarının adedi 32'den fazla olamaz");
         return false;
     }
+    checkIsBinary(fileNames,numberFiles);
     if(calculateSizeofFiles(fileNames,numberFiles)>209715200){
         printf("Giriş dosyalarının toplam boyutu 200 MByte'dan fazla olamaz.");
         return false;
@@ -130,12 +149,9 @@ void zip(IS is){
         writeToFile(fileName,fileNames,numberFiles);
     }
 }
-
-
 void unzip(){
 
 }
-
 void parse_line(IS is){
     if(strcmp(is->fields[0],"tarsau") == 0){
         if(strcmp(is->fields[1],"-a") == 0){
@@ -151,8 +167,6 @@ void parse_line(IS is){
       printf("Lütfen düzgün bir komut giriniz.\n");
     }
 }
-
-
 int main(int argc, char **argv) {
     IS is = new_inputstruct(NULL);
     while (1){
